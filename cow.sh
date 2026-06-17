cf1b592f239bdbee3135baa1c81500612686e279
grishma2477 <kandelgrishma2477@gmail.com>
2026-06-17 21:10:03 +1000

    Bugs Fixed

--- cow.sh
+++ cow.sh
@@ -1,4 +1,4 @@
-for img in frontend paymentservice adservice productcatalogservice cartservice emailservice currencyservice shippingservice recommendationservice loadgenerator shoppingassistantservice 
+for img in frontend paymentservice adservice productcatalogservice cartservice emailservice currencyservice shippingservice recommendationservice loadgenerator shoppingassistantservice checkoutservice
 do 
     docker pull ghcr.io/sulav02/$img:v1.0.0
     docker tag ghcr.io/sulav02/$img:v1.0.0 ghcr.io/grishma2477/ms/$img:1.0.0
